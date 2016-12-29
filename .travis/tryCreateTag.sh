03a4b48a00864ee3082ca9ea761bf1be181a5467
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2016-12-29 20:05:41 +0200

    Made auto-tagging not to depend on HEAD
    using Travis's env variables: TRAVIS_BRANCH, TRAVIS_TRAG and TRAVIS_COMMIT
    to determine when we need to create a tag, and what the build number should be

--- .travis/tryCreateTag.sh
+++ .travis/tryCreateTag.sh
@@ -3,8 +3,12 @@
 export MAJOR_VER=0
 export MINOR_VER=10
 
-export GIT_BRANCH=$(git symbolic-ref $TRAVIS_COMMIT --short)
-if [ "$GIT_BRANCH" == "master" ] && [ -z "$TRAVIS_TAG" ]; then
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_TAG: $TRAVIS_TAG"
+echo "TRAVIS_COMMIT: $TRAVIS_COMMIT"
+
+# Create a tag only for master builds (excluding tag-triggered builds)
+if [ "$TRAVIS_BRANCH" == "master" ] && [ -z "$TRAVIS_TAG" ]; then
     export GIT_BUILDNUM=$(git rev-list --merges --count $TRAVIS_COMMIT)
     export GIT_TAG=v$MAJOR_VER.$MINOR_VER.$GIT_BUILDNUM
     echo "Applying tag $GIT_TAG"
@@ -12,4 +16,7 @@ if [ "$GIT_BRANCH" == "master" ] && [ -z "$TRAVIS_TAG" ]; then
 
     API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Release of version %s","draft": false,"prerelease": false}' $GIT_TAG $GIT_TAG $GIT_TAG)
     curl -s -o /dev/null --data "$API_JSON" https://api.github.com/repos/Sparrow-lang/sparrow/releases?access_token=$GITHUBKEY
+    # This can fail
+    # Please note that we have multiple jobs that may attempt to create the same tag
 fi
+
